9d0c580129ac8f6445dae2422f71854cb58ffda2
Asliddin18 <137496224+odil3474@users.noreply.github.com>
2023-07-05 18:13:43 +0500

    sSQsQS
    SQsqSQsq

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import Profil from './pages/js/profil'
 import Pdp from './pages/js/Pdp'
-
 
 export default function App() {
   return (
     <div>
+     <Profil/>
      <Pdp/>
-     
     </div>
   )
 }
