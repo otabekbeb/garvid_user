66b3a3b8fbeabb75df5bcf8a01f3c6e75f09f4e8
Otabek <130212163+ARTXxXotw@users.noreply.github.com>
2023-07-05 15:45:02 +0500

    qwdqdq
    qeeq2ee

--- src/pages/js/profil.js
+++ src/pages/js/profil.js
@@ -5,25 +5,28 @@ import icon_img1 from "../img/Vector.svg"
 import icon_img2 from "../img/medal.svg"
 import "../css/profil.css"
 import {MdOutlinePhotoCamera} from "react-icons/md"
+import {BsActivity,BsFillBellFill,BsThreeDots} from "react-icons/bs"
 
 export default function Profil() {
 
   function userimgModal(){
-    document.querySelector(".user_img_hover").style="display:block;"
+    document.querySelector(".user_img_hover").style="  position: absolute;bottom: 0;"
     
   }
   function userimgClose(){
-    document.querySelector(".user_img_hover").style="display:none;"
+    document.querySelector(".user_img_hover").style="  position: absolute;bottom: -100px;"
     
   }
 
   return (
     <div>
+      <div className="profil_size_df">
        <div className="profil_size">
         <div className="profil_blok_bir">
           <div onMouseLeave={()=>userimgClose()} className='user_img_size'>
           <img onMouseEnter={()=>userimgModal()} className='user_img' src={Use_img} alt="" />
            <div className="user_img_hover">
+            <input type="file" />
           <MdOutlinePhotoCamera  className='user_hover_photo_icon'/>
         </div>
         </div>
@@ -45,8 +48,23 @@ export default function Profil() {
            </div>
         </div>
         
-        <div className="profil_blok_ikki"></div>
+        <div className="profil_blok_ikki">
+          <div className="profil_blok_ikki_text">
+          <p>Текущий баланс</p>
+          <div className="profil_blok_ikki_sum">
+            <h1>1 350 000</h1><p>UZS</p>
+          </div>
+          <div className="profil_blok_ikki_button">
+            <button><BsActivity/></button><button>Пополнение баланса</button>
+          </div>
+          </div>
+          <div className="profil_blok_ikki_icon">
+          <BsFillBellFill className='profil_blok_ikki_icon_bir' />
+          <BsThreeDots className='profil_blok_ikki_icon_ikki' />
+          </div>
+        </div>
         </div> 
+      </div>
     </div>
   )
 }
