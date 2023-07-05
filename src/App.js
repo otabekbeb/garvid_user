e5966fb225b15769dd536fc4c7b9f2859bf8e182
Otabek <130212163+ARTXxXotw@users.noreply.github.com>
2023-07-05 17:43:02 +0500

    adwdwad
    waddawd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Pdp from './pages/js/Pdp.js'
+import Pdp from './pages/js/profil'
+import Nn from "./pages/js/Pdp"
 
 export default function App() {
   return (
     <div>
       <Pdp/>
+      <Nn/>
     </div>
   )
 }
