545a54d4344696dd29772c7704dad81a1d47318b
Asliddin18 <137496224+odil3474@users.noreply.github.com>
2023-07-06 14:06:15 +0500

    dawwda
    dawdwad

--- src/pages/js/Bilim.js
+++ src/pages/js/Bilim.js
@@ -1,8 +1,64 @@
 import React from 'react'
+import '../css/Bilim.css'
+import Sas from '../img/Sas.png'
+import Htmlimg from '../img/Html.png'
+import Bootstrap from '../img/Bootstrap.png'
+
 export default function Bilim() {
   return (
     <div>
-       Bilim
+     <div className="program">
+        <div className="ptogram_tutorial">
+            <div className="html_code_tutoril">
+            <div className="html_div">
+                <div className="html_red">
+                    <img src={Htmlimg} alt="" />
+                </div>  
+            </div>
+            <div className="text_html">
+            <h3>HTML</h3>
+            <p>Lorem ipsum dolor sit 
+amet, consectetur ar.</p>
+            </div>
+            </div>
+            <div className="html_code_tutoril">
+            <div className="html_div">
+                <div className="sas_red">
+                    <img src={Sas} alt="" />
+                </div>  
+            </div>
+            <div className="text_html">
+            <h3>Sass</h3>
+            <p>Lorem ipsum dolor sit 
+amet, consectetur ar.</p>
+            </div>
+            </div>
+            <div className="html_code_tutoril">
+            <div className="html_div">
+                <div className="bootstrap_red">
+                    <img src={Bootstrap} alt="" />
+                </div>  
+            </div>
+            <div className="text_html">
+            <h3>Bootstrap</h3>
+            <p>Lorem ipsum dolor sit 
+amet, consectetur ar.</p>
+            </div>
+            </div>
+            <div className="html_code_tutoril">
+            <div className="html_div">
+                <div className="bootstrap_red">
+                    <img src={Bootstrap} alt="" />
+                </div>  
+            </div>
+            <div className="text_html">
+            <h3>GIT</h3>
+            <p>Lorem ipsum dolor sit 
+amet, consectetur ar.</p>
+            </div>
+            </div>
+        </div>
+     </div>
     </div>
   )
 }
