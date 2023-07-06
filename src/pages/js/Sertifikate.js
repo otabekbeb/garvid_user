bdace97639b59d67b3bcfc38e75bc61f4f7bbbfd
Asliddin18 <137496224+odil3474@users.noreply.github.com>
2023-07-06 17:57:57 +0500

    lavash
    lavash

--- src/pages/js/Sertifikate.js
+++ src/pages/js/Sertifikate.js
@@ -1,10 +1,69 @@
 import React from 'react'
 import '../css/Sertifikate.css'
+import Sertifikat from '../img/Sertifikat.png'
+import Start from '../img/Start.png'
 export default function Sertifikate() {
   return (
     <div>
+        <div className="cards_sertifikat">
+            <div className="card_sertifikat">
+            <div className="sertifikat">
+                <img src={Sertifikat} alt="" />
+            </div>
+            <div className="sertifikat">
+                <img src={Start} alt="" />
+                <div className="button_text_df_blok_pitani_pro_Max">
+                    <div className="text_pro_max_gap_sos_lift_gr">
+                    <h4>Raspberry Pi dasturlash asoslari</h4>
+                    <p>20-iyun, 2019 y</p>
+                    </div>
+                    <button className='Dast_konter_dust_pro_dest'>Dasturlash</button>
+                </div>
+            </div>
+            <div className="sertifikat">
+                <img src={Start} alt="" />
+                <div className="button_text_df_blok_pitani_pro_Max">
+                    <div className="text_pro_max_gap_sos_lift_gr">
+                    <h4>Raspberry Pi dasturlash asoslari</h4>
+                    <p>20-iyun, 2019 y</p>
+                    </div>
+                    <button className='Dast_konter_dust_pro_dest'>Dasturlash</button>
+                </div>
+            </div>
+            <div className="sertifikat">
+                <img src={Start} alt="" />
+                <div className="button_text_df_blok_pitani_pro_Max">
+                    <div className="text_pro_max_gap_sos_lift_gr">
+                    <h4>Raspberry Pi dasturlash asoslari</h4>
+                    <p>20-iyun, 2019 y</p>
+                    </div>
+                    <button className='Dast_konter_dust_pro_dest'>Dasturlash</button>
+                </div>
+            </div>
+            <div className="sertifikat">
+                <img src={Start} alt="" />
+                <div className="button_text_df_blok_pitani_pro_Max">
+                    <div className="text_pro_max_gap_sos_lift_gr">
+                    <h4>Raspberry Pi dasturlash asoslari</h4>
+                    <p>20-iyun, 2019 y</p>
+                    </div>
+                    <button className='Dast_konter_dust_pro_dest'>Dasturlash</button>
+                </div>
+            </div>
+            <div className="sertifikat">
+                <img src={Start} alt="" />
+                <div className="button_text_df_blok_pitani_pro_Max">
+                    <div className="text_pro_max_gap_sos_lift_gr">
+                    <h4>Raspberry Pi dasturlash asoslari</h4>
+                    <p>20-iyun, 2019 y</p>
+                    </div>
+                    <button className='Dast_konter_dust_pro_dest'>Dasturlash</button>
+                </div>
+            </div>
+            
+            </div>
+        </div>
 
-        
     </div>
   )
 }
