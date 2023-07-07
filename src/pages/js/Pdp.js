4c968a3148fda7afde3719342a406410bd9faee1
Otabek <130212163+ARTXxXotw@users.noreply.github.com>
2023-07-07 14:02:20 +0500

    dwada
    ddaddad

--- src/pages/js/Pdp.js
+++ src/pages/js/Pdp.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import '../css/Pdp.css'
-import Nav from 'react-bootstrap/Nav';
 import Filtr from "./Searchfilter"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Bilim from './Bilim'
-import Sertifikat from '../js/Sertifikate'
+import Sertifikat from './Sertifikate'
+import Azo from "./azo"
 // import Mon from '../img/Mon.png'
 // import { AiFillStar } from 'react-icons/ai'
 // import {HiArrowRight} from 'react-icons/hi'
@@ -26,10 +26,10 @@ export default function Pdp() {
         <div>
             <div className="gray_blok">
                 <div className="fil_text_blok">
-                <h1 onClick={()=>updatetoggle(1)} className='fromLeft'>Мои курсы</h1>
-                <h1 onClick={()=>updatetoggle(2)} className='fromLeft'>Мои усвоенные знания</h1>
-                <h1 onClick={()=>updatetoggle(3)} className='fromLeft'>Мои сертификаты</h1>
-                <h1 onClick={()=>updatetoggle(4)} className='fromLeft'>Мои наставники-члены</h1>
+                <div className='fil_text_blok_soz'><h1 onClick={()=>updatetoggle(1)} className='fromLeft'>Мои курсы</h1><div className="fil_text_blok_kurs_lenght">4 штуки</div></div>
+                <div className='fil_text_blok_soz'><h1 onClick={()=>updatetoggle(2)} className='fromLeft'>Мои усвоенные знания</h1><div className="fil_text_blok_kurs_lenght">14 штуки</div></div>
+                <div className='fil_text_blok_soz'><h1 onClick={()=>updatetoggle(3)} className='fromLeft'>Мои сертификаты</h1><div className="fil_text_blok_kurs_lenght">24 штуки</div></div>
+                <div className='fil_text_blok_soz'><h1 onClick={()=>updatetoggle(4)} className='fromLeft'>Мои наставники-члены</h1><div className="fil_text_blok_kurs_lenght">24 штуки</div></div>
                 </div>
                 <div className="profil_blok_menu_size">
                 <TiThMenu onClick={()=>menuModal()} className='profil_blok_menu'/>
@@ -45,7 +45,7 @@ export default function Pdp() {
             <div className={toggle === 1 ? "show-content" : "content"}><Filtr /></div>
             <div className={toggle === 2 ? "show-content" : "content"}><Bilim/></div>
             <div className={toggle === 3 ? "show-content" : "content"}><Sertifikat/> </div>
-            <div className={toggle === 4 ? "show-content" : "content"}>azolarim</div>
+            <div className={toggle === 4 ? "show-content" : "content"}><Azo/></div>
 
         </div>
     )
---
+++ src/pages/js/azo.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import "../css/azo.css"
+import azoimg from "../img/Ellipse.jpg"
+
+export default function azo() {
+  return (
+    <div>
+    <div className="azo_bolgan_katta">
+        <div className="azo_bolgan_katta_div">
+            <div className='azo_blogan_katta_img'>
+            <img src={azoimg} alt="" />
+            </div>
+            <div className="azo_bolgan_katta_div_text">
+                <div className="azo_bolgan_katta_div_text_block">
+                    <h1>Диана Генри</h1>
+                    <p>Разработчик Java, Python</p>
+                </div>
+                <button>Отмена членства</button>
+            </div>
+        </div>
+        <div className="azo_bolgan_katta_div">
+            <div className='azo_blogan_katta_img'>
+            <img src={azoimg} alt="" />
+            </div>
+            <div className="azo_bolgan_katta_div_text">
+                <div className="azo_bolgan_katta_div_text_block">
+                    <h1>Диана Генри</h1>
+                    <p>Разработчик Java, Python</p>
+                </div>
+                <button>Отмена членства</button>
+            </div>
+        </div>
+        <div className="azo_bolgan_katta_div">
+            <div className='azo_blogan_katta_img'>
+            <img src={azoimg} alt="" />
+            </div>
+            <div className="azo_bolgan_katta_div_text">
+                <div className="azo_bolgan_katta_div_text_block">
+                    <h1>Диана Генри</h1>
+                    <p>Разработчик Java, Python</p>
+                </div>
+                <button>Отмена членства</button>
+            </div>
+        </div>
+        <div className="azo_bolgan_katta_div">
+            <div className='azo_blogan_katta_img'>
+            <img src={azoimg} alt="" />
+            </div>
+            <div className="azo_bolgan_katta_div_text">
+                <div className="azo_bolgan_katta_div_text_block">
+                    <h1>Диана Генри</h1>
+                    <p>Разработчик Java, Python</p>
+                </div>
+                <button>Отмена членства</button>
+            </div>
+        </div>
+        <div className="azo_bolgan_katta_div">
+            <div className='azo_blogan_katta_img'>
+            <img src={azoimg} alt="" />
+            </div>
+            <div className="azo_bolgan_katta_div_text">
+                <div className="azo_bolgan_katta_div_text_block">
+                    <h1>Диана Генри</h1>
+                    <p>Разработчик Java, Python</p>
+                </div>
+                <button>Отмена членства</button>
+            </div>
+        </div>
+    </div>
+    </div>
+  
+)}
