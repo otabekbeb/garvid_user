73e07c86995418faf6dcdb7f70067ca558562b10
Asliddin18 <137496224+odil3474@users.noreply.github.com>
2023-07-08 16:48:42 +0500

    odil
    odil

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
+
 import Profil from './pages/js/Profil'
 import Pdp from './pages/js/Pdp'
 import Mentor from './pages/js/Mentor'
 import MentorPdp from "./pages/js/MentorPdp"
-
+import Navbar from   './pages/js/Navbar'
 export default function App() {
   return (
     <div>
       {/* <Mentor/> */}
 
-          {/* <Navbar /> */}
+
+          <Navbar />
      {/* <Profil/> */}
      {/* <Pdp/> */}
 
