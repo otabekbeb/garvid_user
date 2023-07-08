6d1177b5eaca4dc089349915bb188f43cca1f861
Asliddin18 <137496224+odil3474@users.noreply.github.com>
2023-07-08 13:24:49 +0500

    1111
    1111

--- src/pages/js/Navbar.js
+++ src/pages/js/Navbar.js
@@ -2,38 +2,75 @@ import React from 'react'
 import Pdp from '../img/Pdp.png'
 import Elp from '../img/Ellipse.jpg'
 import '../css/Navbar.css'
-import {BiChevronDown} from 'react-icons/bi'
+import { BiChevronDown } from 'react-icons/bi'
 export default function Navbar() {
-    
-    function Down(){
+
+    function Down() {
         document.querySelector(".profil_menu_modal").classList.toggle("db")
+
+ }
+    function Foto() {
+        document.querySelector(".profil_name").classList.toggle("dode");
+        document
     }
-       
     return (
-    <div>
-        <div className="Navbar_df_flex">
-            <div className="img_navbar_img">
-                <img src={Pdp} alt="" />
+        <div>
+            <div className="navbar">
+                <div className="Navbar_df_flex">
+
+                    <div className="img_navbar_img">
+                        <img src={Pdp} alt="" />
+                    </div>
+                    <div className="profil_profil">
+                        <div className="profil_emile">
+                            <div className="profil_img" onClick={() => { Foto() }}>
+                                <img src={Elp} alt="" />
+                            </div>
+                            <div className="profil_text">
+                                <h3>To’rayev Ja’farbek</h3>
+                                <p>Oddiy foydalanuvchi</p>
+
+                            </div>
+                        </div>
+                        <BiChevronDown  onClick={() => { Down() }} className='Down_icon' />
+                        <div className="profil_menu_modal">
+                            <div className="Front_div">
+                                <h3>Front End Kursi</h3>
+                            </div>
+                            <div className="Modx_div">
+                                <h3>Modx Kursi</h3>
+                            </div>
+                            <div className="Bac_div">
+                                <h3>Backend Kursi</h3>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className="profil_name">
+                    <div className="profil_text_name">
+                        <div className="tex_name">
+                            <h3>To’rayev Ja’farbek</h3>
+                            <p>Oddiy foydalanuvchi</p>
+                        </div>
+                        <img className='elp_img_profil' src={Elp} alt="" />
+                    </div>
+                    <div className="profil_menu">
+                        <div className="Front_div">
+                            <h3>Front End Kursi</h3>
+                        </div>
+                        <div className="Modx_div">
+                            <h3>Modx Kursi</h3>
+                        </div>
+                        <div className="Bac_div">
+                            <h3>Backend Kursi</h3>
+                        </div>
+
+                    </div>
+                </div>
             </div>
-            <div className="profil_profil">
-                <div className="profil_emile">
-                <div className="profil_img">
-                    <img src={Elp} alt="" />
-                </div>
-                <div className="profil_text">
-                    <h3>To’rayev Ja’farbek</h3>
-                    <p>Oddiy foydalanuvchi</p>
-                    
-                </div>
-                </div>
-                <BiChevronDown onClick={()=>{Down()}}  className='Down_icon'/>
-                <div className="profil_menu_modal">
-                    awdadwadwad
-                </div>
-            </div>
-            
+
         </div>
-        
-    </div>
-  )
+    )
 }
