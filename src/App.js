28863a39f6593ff1ac69370c7b29ff10b58c99ca
wayne <104674638+umarofWayne@users.noreply.github.com>
2023-07-10 18:31:31 +0500

    dfdf
    fddffdfdd

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Pdp from './pages/js/Pdp'
 import Mentor from './pages/js/Mentor'
 import MentorPdp from "./pages/js/MentorPdp"
 import Navbar from   './pages/js/Navbar'
+import YouTube from   './pages/js/Youtube1'
 export default function App() {
   return (
     <div>
-      <Profil/>
-      <Pdp/>
+      {/* <Profil/>
+      <Pdp/> */}
+      <YouTube/>
     </div>
   )
 }
