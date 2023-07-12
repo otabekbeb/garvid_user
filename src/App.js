8c6c25bd490308edd1bea376dfa12232f8d61c01
Asliddin18 <137496224+odil3474@users.noreply.github.com>
2023-07-12 13:12:46 +0500

    9999
    999999999

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Pdp from './pages/js/Pdp'
 import Mentor from './pages/js/Mentor'
 import MentorPdp from "./pages/js/MentorPdp"
 import Navbar from   './pages/js/Navbar'
-
+import Vazifa from './pages/js/Vazifa'
 export default function App() {
   return (
     <div>
 
-       <Profil/>
-      <Pdp/> 
+       {/* <Profil/> */}
+      {/* <Pdp/>  */}
       {/* <Mentor/> */}
-     
+      <Vazifa/>
     </div>
   )
 }
