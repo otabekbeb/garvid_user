3620917d018145e4bc273e8b76a6a96cc0d17fa8
bot1rovx <xojakbarazm@gmail.com>
2023-07-13 06:08:26 -0700

    asdsd
    cascascs

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Pdp from './pages/js/Pdp'
 import Mentor from './pages/js/Mentor'
 import MentorPdp from "./pages/js/MentorPdp"
 import Navbar from   './pages/js/Navbar'
+import Futer from './pages/js/Futer'
 
 import YouTube from   './pages/js/Youtube1'
 export default function App() {
   return (
     <div>
       <Mentor/>
+      <Futer/>
       {/* <Profil/> 
        <Pdp/> */}
     </div>
---
+++ src/pages/js/Futer.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import '../css/Futer.css'
+import {FaTelegramPlane} from "react-icons/fa"
+import {GrFacebookOption,GrYoutube} from "react-icons/gr"
+import {AiFillInstagram} from "react-icons/ai"
+import tolov from '../img/Group 26.png'
+
+export default function Futer() {
+  return (
+    <div>
+        <div className="line1"></div>
+        <div className="futer_botom">
+            <div className="futer_container">
+            <div className="futer_row">
+    <div className="futer_col">
+    <ul><h4>Основной</h4>
+      <li>О проекте</li>
+      <li>Курсы</li>
+      <li>Наставникам</li>
+      <li>Публичная оферта</li>
+      <li>Политика конфиденциальности</li>
+      <div className="futer_i">
+<div className="social_i"><FaTelegramPlane /></div>
+<div className="social_i"><GrFacebookOption /></div>
+<div className="social_i"><AiFillInstagram /></div>
+<div className="social_i"><GrYoutube /></div>
+</div>
+    </ul>
+    
+  </div>
+
+  <div className="futer_col">
+    <ul><h4>Дополнительные</h4>
+      <li>Наша команда</li>
+      <li>Сотрудничество с нами</li>
+      <li>Как это работает?</li>
+    </ul>
+  </div>
+
+  <div className="futer_col">
+    <ul><h4>Поддерживать</h4>
+      <li>Наиболее часто задаваемые вопросы</li>
+      <li>Контакт</li>
+    </ul>
+  </div>
+
+  <div className="futer_col">
+    <ul>
+        <h4>Виды оплаты</h4>
+        <div className="tolov_"><img src={tolov} alt="" /></div>
+    <p>100011, г.Ташкент, Шайхонтохурский район,
+    <br />ул.Заргайнар, дом 3Б</p>
+    <button className='aloqa_btn'>
+Связаться с нами</button>
+    </ul>
+   
+    
+  </div>
+            </div>
+            </div>
+            
+
+
+
+
+<div className="botom_p">
+<p>© 2019 storm.uz</p>
+<div className="mini_line"></div>
+<p>Personel Development Process LLC</p>
+<div className="mini_line"></div>
+<p>Foydalanish shartlari</p>
+</div>
+
+    </div>
+    </div>
+  )
+}
