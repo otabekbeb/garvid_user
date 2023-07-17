3d95e1cea8c7df4795d5da1ae390d9b42f341622
Otabek <130212163+ARTXxXotw@users.noreply.github.com>
2023-07-17 15:19:27 +0500

    wadawd
    adwdda

--- src/pages/js/Vazifa1.js
+++ src/pages/js/Vazifa1.js
@@ -1,68 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../css/vazifa1.css'
 import {BsCheck2, BsArrowDownShort} from 'react-icons/bs'
 import {AiOutlineFileText, AiOutlinePaperClip} from 'react-icons/ai'
+import axios from 'axios'
+import url from './Host'
 
 export default function Vazifa1() {
+  const [task,setTask]=useState([])
+  const [theme,setTheme]=useState([])
+
+  useEffect(()=>{
+    axios.get(`${url}/course/theme_task/`,{headers:{"Accept-Language":"en"}}).then(res=>{
+      console.log(res.data,"zor");
+      setTask(res.data)
+      axios.get(`${url}/course/theme/`,{headers:{"Accept-Language":"en"}}).then(res1=>{
+        setTheme(res1.data)
+      })
+    })
+  },[])
+
   return (
     <div className='m_vazifa_kotta'> 
         <div className="m_vazifa_scachat">
-          <div className="scachat_kotta">
-            <div className="m_zadaniya_s">
-              <input type="checkbox" id='checkbox1'/>
-                <div className="m_scachat"><BsArrowDownShort /></div>
-                <div className="m_title_scac">
-                   <h5>Задачи по переменным.txt</h5>
-                <p >5.1 MB | 25.06.2019</p></div>
-               
-            </div>
-           <div className='checed_vazifa'>
-            <h5>Отправленный файл</h5>
-            <div className="chec_proveren"><BsCheck2/></div>
-           </div>
-           </div>
-           <div className="scachat_kotta">
-            <div className="m_zadaniya_s">
-              <input type="checkbox" id='checkbox1'/>
-                <div className="m_scachat"><BsArrowDownShort /></div>
-                <div className="m_title_scac">
-                   <h5>Задачи по переменным.txt</h5>
-                <p >5.1 MB | 25.06.2019</p></div>
-               
-            </div>
-           <div className='checed_vazifa'>
-            <h5>Отправленный файл</h5>
-            <div className="chec_proveren"><BsCheck2/></div>
-           </div>
-           </div>
-           <div className="scachat_kotta">
-            <div className="m_zadaniya_s">
-              <input type="checkbox" id='checkbox1'/>
-                <div className="m_scachat"><BsArrowDownShort /></div>
-                <div className="m_title_scac">
-                   <h5>Задачи по переменным.txt</h5>
-                <p >5.1 MB | 25.06.2019</p></div>
-               
-            </div>
-           <div className='checed_vazifa'>
-            <h5>Отправленный файл</h5>
-            <div className="chec_proveren"><BsCheck2/></div>
-           </div>
-           </div>
-           <div className="scachat_kotta">
-            <div className="m_zadaniya_s">
-              <input type="checkbox" id='checkbox1'/>
-                <div className="m_scachat"><BsArrowDownShort /></div>
-                <div className="m_title_scac">
-                   <h5>Задачи по переменным.txt</h5>
-                <p >5.1 MB | 25.06.2019</p></div>
-               
-            </div>
-           <div className='checed_vazifa'>
-            <h5>Отправленный файл</h5>
-            <div className="chec_proveren"><BsCheck2/></div>
-           </div>
-           </div>
+          {theme.map(item=>{
+            return(
+              <>
+              {task.map(item1=>{
+              if (item.id===item1.course_theme) {
+              return(
+                <div className="scachat_kotta">
+                <div className="m_zadaniya_s">
+                  <input type="checkbox" id='checkbox1'/>
+                    <div className="m_scachat"><BsArrowDownShort /></div>
+                    <div className="m_title_scac">
+                       <h5>{item1.content}</h5>
+                    <p >5.1 MB | 25.06.2019</p></div>
+                   
+                </div>
+               <div className='checed_vazifa'>
+                <h5>Отправленный файл</h5>
+                <div className="chec_proveren"><BsCheck2/></div>
+               </div>
+               </div>  
+              )            
+              }
+              })}
+              </>
+            )
+          })}
 
 
 
