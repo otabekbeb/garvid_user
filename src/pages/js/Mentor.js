18a809f967cd5aeaa338e80dc1471c6ccd79b122
Od1l <alautdinovodil3474@gmail.com>
2023-07-20 11:06:57 +0500

    ddc
    deef

--- src/pages/js/Mentor.js
+++ src/pages/js/Mentor.js
@@ -2,14 +2,14 @@ import React from 'react'
 import MentorProfil from "./Mentorprofile"
 import MentorPdp from "./MentorPdp"
 import Futer from "./Futer"
-
+import Navbar from './Navbar'
 export default function Mentor() {
   return (
     <div>
-      
+      <Navbar/>
       <MentorProfil/>
       <MentorPdp/>
-    <Futer/>
+      <Futer/>
     </div>
   )
 }
