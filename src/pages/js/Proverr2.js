a017a348f1a65ee1a3d3057267ee9ed685dce84f
mr1github07 <132270932+abzalbeb@users.noreply.github.com>
2023-07-22 13:40:27 +0300

    asd
    asd

--- src/pages/js/Proverr2.js
+++ src/pages/js/Proverr2.js
@@ -255,19 +255,19 @@ dolor sit amet. . . .</p>
 
         <div className={toggle===1?"text-kurs-haqida3":"text-kurs-haqida2"}><div className="text-kurs-haqida1">
 <p>
-This course starts from scratch, you neither need to know Angular 1 nor Angular 2! Angular 8 simply is the latest <br /> version of Angular 2.
+This course starts from scratch, you neither need to know Angular 1 nor Angular 2! Angular 8 simply is the latest <br className="poopoasd" /> version of Angular 2.
 
 
-Join the most comprehensive and popular Angular course on Udemy, because now is the <br /> time to get started!From Setup to Deployment, this course covers it all! You'll learn all about Components, <br /> Directives, Services, Forms, Http Access, Authentication, Optimizing an Angular App with Modules and Offline <br /> Compilation and much more - and in the end: You'll learn how to deploy an application!But that's not all! <br /> This course will also show you how to use the Angular CLI and feature a complete project, which allows you <br /> to practice the things learned throughout the course!
+Join the most comprehensive and popular Angular course on Udemy, because now is the <br className="poopoasd" /> time to get started!From Setup to Deployment, this course covers it all! You'll learn all about Components, <br className="poopoasd" /> Directives, Services, Forms, Http Access, Authentication, Optimizing an Angular App with Modules and Offline <br className="poopoasd" /> Compilation and much more - and in the end: You'll learn how to deploy an application!But that's not all! <br className="poopoasd" /> This course will also show you how to use the Angular CLI and feature a complete project, which allows you <br className="poopoasd" /> to practice the things learned throughout the course!
 
-And if you do get stuck, you benefit from an  <br />extremely fast and friendly support - both via direct messaging or discussion. You have my word! ;-) <br />
+And if you do get stuck, you benefit from an  <br className="poopoasd" />extremely fast and friendly support - both via direct messaging or discussion. You have my word! ;-) <br className="poopoasd" />
 
-Angular is one of the most modern, performance-efficient and powerful frontend frameworks you can <br /> learn as of today. It allows you to build great web apps which offer awesome user experiences! Learn <br /> <span id="prover5-info-block1">
-all the fundamentals you need to know to get started developing Angular <br /> applications right away.
+Angular is one of the most modern, performance-efficient and powerful frontend frameworks you can <br className="poopoasd" /> learn as of today. It allows you to build great web apps which offer awesome user experiences! Learn <br className="poopoasd" /><span id="prover5-info-block1">
+all the fundamentals you need to know to get started developing Angular <br className="poopoasd" /> applications right away.
 
 Hear what my students have to say
 
-Absolutely fantastic tutorial <br /> series. I cannot thank you enough. The quality is first class and your presentational skills are second to none. Keep <br /> up this excellent work. You really rock!﻿ - Paul Whitehouse
+Absolutely fantastic tutorial <br className="poopoasd" /> series. I cannot thank you enough. The quality is first class and your presentational skills are second to none. Keep <br /> up this excellent work. You really rock!﻿ - Paul Whitehouse
 </span>
 
 </p>
@@ -432,7 +432,203 @@ Absolutely fantastic tutorial <br /> series. I cannot thank you enough. The qual
         </div>
         
     </div>
-  <div className="mni-grid-pas-media">ierhogrigrhghgeairhgpaeiirhrhgpeirhgiehrhgeiarhngkiidg</div>
+    <div className="mni-orta-bloxk-mediaa">
+        <div className="mni-orta-kota-media">
+        <div className="mni-media-orta-input">
+        <form action="">
+           <input type="text" placeholder='Какой курс вы хотите изучать?' required /><button><box-icon name='search' color='#9da7bb' ></box-icon></button>
+           </form>
+        </div>
+        <p></p>
+        <p></p>
+        <div className="mni-media-orta-filter">
+        <box-icon name='chevron-down' color='#c5c9d3' ></box-icon>
+        </div>
+
+        </div>
+        <div className="prover3-search-joy">
+        <div className="prover3-mni-search">
+           <div className="prover2-info-d">
+            <div className="prover2-info-filter">
+          <button className='prover2-but-clas'><p>#Дизайн</p></button>
+          <button className='prover2-but-clas2'><p>#Маркетинг</p></button>
+          <button className='prover2-but-clas3'><p>#Бизнес</p></button>
+          <button className='prover2-but-clas4'><p>#Интернет</p></button>
+            </div>
+            <div className="prover2-info-filter2">
+                <button className='prover2-but-clas5'><p>#Бухгалтерский</p></button>
+                <button className='prover2-but-clas6'><p>#Здоровье</p></button>
+                <button className='prover2-but-clas7'><p>#Программирование</p></button>
+                <button className='prover2-but-clas7'><p>#питание</p></button>
+            </div>
+            <div className="prover2-info-youtube-f">
+                <p className='prover2-p-df'>Программирование</p>
+                <div className="prover2-info-block1">
+                    <div className="prover2-info-block1-img">
+                        <img src={rasmww} alt="" />
+                    </div>
+                    <div className="prover2-info-block1-text">
+                        <h5>Создание RESTful API <br className="prover1-info-none-br" />
+практические задания на</h5>
+<p>Lorem ipsum dolor sit amet, consectetur <br />
+dolor sit amet. . . .</p>
+                    </div>
+                </div>
+                <div className="prover2-linerr1">
+
+                </div>
+                <div className="prover3-info-block1">
+                    <div className="prover3-info-block1-img">
+                        <img src={rasmww} alt="" />
+                    </div>
+                    <div className="prover3-info-block1-text">
+                       <h5>AWS для разработчиков:  <br className="prover1-info-none-br" />
+ECS и мультирегиональные<br className="prover1-info-none-br" /> перевозки. . .</h5>
+<p>Lorem ipsum dolor sit amet, consectetur <br />
+dolor sit amet. . . .</p>
+                    </div>
+                </div>
+                <div className="prover2-linerr1">
+
+                </div>
+
+                <div className="prover4-info-block1">
+                    <div className="prover4-info-block1-img">
+                        <img src={iphone} alt="" />
+                    </div>
+                    <div className="prover4-info-block1-text">
+                       <h5>Программирование на iOS <br className="prover1-info-none-br" />
+курсы</h5>
+<p>Lorem ipsum dolor sit amet, consectetur <br />
+dolor sit amet. . . .</p>
+                    </div>
+                </div>
+                <div className="prover2-linerr1">
+
+                </div>
+                <div className="prover4-info-block1">
+                    <div className="prover4-info-block1-img">
+                        <img src={iphone} alt="" />
+                    </div>
+                    <div className="prover4-info-block1-text">
+                       <h5>Программирование на iOS <br className="prover1-info-none-br" />
+курсы</h5>
+<p>Lorem ipsum dolor sit amet, consectetur <br />
+dolor sit amet. . . .</p>
+                    </div>
+                </div>
+                <div className="prover2-linerr1">
+
+                </div>
+                <div className="prover4-info-block1">
+                    <div className="prover4-info-block1-img">
+                        <img src={iphone} alt="" />
+                    </div>
+                    <div className="prover4-info-block1-text">
+                       <h5>Программирование на iOS <br className="prover1-info-none-br" />
+курсы</h5>
+<p>Lorem ipsum dolor sit amet, consectetur <br />
+dolor sit amet. . . .</p>
+                    </div>
+                </div>
+                <div className="prover3-linerr1">
+
+                </div>
+                
+                <div className="prover5-info-block1">
+                    <div className="prover5-info-block1-img">
+                        <img src={rasmww} alt="" />
+                    </div>
+                    <div className="prover5-info-block1-text">
+                        <h5>Создание RESTful API <br className="prover1-info-none-br" />
+практические задания на</h5>
+<p>Lorem ipsum dolor sit amet, consectetur <br />
+dolor sit amet. . . .</p>
+                    </div>
+                </div>
+
+            </div>
+            
+           </div>
+        </div>
+        </div>
+    </div>
+  <div className="mni-grid-pas-media">
+
+  <div className="prover2-oxiri-block-in">
+            <div className="mni-dasturlash-bloc"><p>Программирование</p></div>
+            <div className="mni-kurs-narxi">
+                <div className="mni-kurs-block1"><h5>Стоимость курса</h5>
+                <p>250.000 <span>RUB</span></p></div>
+                <div className="mni-kurs-block2"></div>
+                
+                <div className="mni-kurs-block1"><h5>Kurs hajmi</h5>
+                <p>38 soat</p></div>
+                <div className="mni-kurs-block2"></div>
+            </div>
+            <p className="spdfodsofdsf">Разделы курса:</p>
+            <div className="mni-blocc-linee"></div>
+            
+<div className="prover2-oxiri-total-kurss"onMouseLeave={()=>aboutClose123122()}  >
+<div className="prover20chech-box-a" onMouseEnter={()=>about2222()} onClick={()=>akkordion()} >
+    <input type="checkbox"/>
+    <p>Введение в процесс программирования и установки</p>
+    <span><box-icon name='chevron-down' color='#989da2' ></box-icon></span>
+</div>
+<div className="mni-kurs-narxi2">
+                <div className="mni-kurs-block1"><h5>Стоимость курса</h5>
+                <p>150.000  <span>RUB</span></p></div>
+                <div className="mni-kurs-block2"></div>
+                
+                <div className="mni-kurs-block1"><h5>Размер поля</h5>
+                <p>38 час</p></div>
+                <div className="mni-kurs-block2"></div>
+            </div>
+</div>
+<div className="mni-accordion-none1">
+    <div className="prover20chech-box-a2">
+        <input type="checkbox" />
+        <p>Работа с простыми операциями в программе</p>
+        <span><box-icon name='chevron-down' color='#989da2' ></box-icon></span>
+    </div>
+    <div className="prover20chech-box-a2">
+        <input type="checkbox" />
+        <p>Работа с простыми операциями в программе</p>
+        <span><box-icon name='chevron-down' color='#989da2' ></box-icon></span>
+    </div>
+</div>
+<div className="mni-buton-iikki-yo">
+    <button id="gbfdgfdgdfgdf">Покупка</button><div className="line-mni-but"></div><button >150 000 сум</button>
+</div>
+      
+            </div>
+      <div className="mni-krus-techer-swiper">
+<div className="boshqa-mentorla">
+    <h5>Boshqa Mentorlar</h5>
+    <div className="boshqa-mentorla-krugg1">
+        <div className="boshqa-kurglaaaaa1" onClick={()=>onga()}><box-icon name='right-arrow-alt' color='#536dfd' ></box-icon></div>
+    </div>
+</div>
+<div className="mni-swiper-grid">
+    <div className="mni-gridf1">
+<img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/46f705111077375.5ffb645951e7c.jpg" alt="" />
+<img src="https://ag-spots-2020.o.auroraobjects.eu/2020/08/22/other/2880-1800-crop-bmw-m5-f90-competition-2021-c330122082020014437_1.jpg" alt="" />
+<img src="https://ag-spots-2020.o.auroraobjects.eu/2020/03/07/other/2880-1800-crop-bmw-m5-f90-competition-c490507032020171519_1.jpg" alt="" />
+<img src="https://ag-spots-2021.o.auroraobjects.eu/2021/01/17/bmw-m5-f90-competition-2021-c451117012021040035_1.jpg" alt="" />
+    </div>
+    
+    <div className="mni-gridf2">
+<img src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg" alt="" />
+<img src="https://bronk.club/uploads/posts/2023-02/1676935060_bronk-club-p-otkritki-prirodi-ochen-krasivie-krasivo-39.jpg" alt="" />
+<img src="https://funart.pro/uploads/posts/2021-03/thumbs/1617041574_9-p-oboi-krasivie-foto-prirodi-11.jpg" alt="" />
+<img src="https://cdn.forbes.ru/forbes-static/c/1040x549/new/2023/04/1GettyImages-183930658-kopia-643018c796355.webp" alt="" />
+
+    </div>
+</div>
+</div>
+
+    
+  </div>
 </div>
 
 
