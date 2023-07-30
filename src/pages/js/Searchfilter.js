50ed2df1a3714f82bdd28d992009b5316e88b9f4
bel1y <132271045+bel1y@users.noreply.github.com>
2023-07-30 15:25:23 +0500

    dsfsf
    sdfs

--- src/pages/js/Searchfilter.js
+++ src/pages/js/Searchfilter.js
@@ -50,11 +50,13 @@ export default function Searchfilter() {
     setState1(
       localStorage.getItem("lang") ? localStorage.getItem("lang") : "en"
     );
+
+    axios.ger(`${url}`)
     setLoader(1)
   }, []);
   function filter (id) {
     axios
-    .get(`${url}/course/main/`, { headers: { "Accept-Language": localStorage.getItem("lang") ? localStorage.getItem("lang") : "en" } })
+    .get(`${url}/api/course`, { headers: { "Accept-Language": localStorage.getItem("lang") ? localStorage.getItem("lang") : "en" } })
     .then((res) => {
       const search = res.data.filter(item=>item.course_type===id)
       setKursdata(search)
@@ -62,7 +64,7 @@ export default function Searchfilter() {
   }
   const searchInput = (event) => {
     const searchRegex = new RegExp(`^${event.target.value}`, "i");
-    axios.get(`${url}/course/main/`,{ headers: { "Accept-Language": localStorage.getItem("lang") ? localStorage.getItem("lang") : "en" } }).then(res=>{
+    axios.get(`${url}/api/course`,{ headers: { "Accept-Language": localStorage.getItem("lang") ? localStorage.getItem("lang") : "en" } }).then(res=>{
       const searchdata = res.data.filter((item) => {
         return (
           searchRegex.test(item.name) 
