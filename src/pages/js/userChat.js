9e0260510a6e58051719e6e1d99559a1c3ab4aab
Akbar-07 <116638921+Akbar-07@users.noreply.github.com>
2023-08-09 13:55:16 +0300

    aa
    aa

--- src/pages/js/userChat.js
+++ src/pages/js/userChat.js
@@ -15,15 +15,14 @@ export default function MentorChat() {
   const [users, setUsers] = useState([]);
   const [theme,setTheme] = useState(localStorage.getItem("back"))
   useEffect(() => {
-    if(localStorage.getItem("back")!=="sun"){
-      document.querySelector("#checkbox2").checked=true
+    if(localStorage.getItem("back")!=="moon"){
+      document.querySelector("#checkbox2").checked=false
       }else{
-        document.querySelector("#checkbox2").checked=false
+        document.querySelector("#checkbox2").checked=true
       };
     setState1(
       localStorage.getItem("lang") ? localStorage.getItem("lang") : "en"
-    );
-    axios
+    );    axios
       .get("https://markazback2.onrender.com/auth/oneuser", {
         headers: { Authorization: "Bearer " + localStorage.getItem("token") },
       })
@@ -108,57 +107,60 @@ export default function MentorChat() {
       setUsers(searchdata);
     });
   };
+  function telegrammenu() {
+    
+  }
   function chatModal(room) {
     setRoom(room);
     socket.emit("join_room", { email, room });
     document.querySelector(".yozishma_bolim_text_nik").style = "display:none;";
-    if (theme=="sun") {
-      document.querySelector(".yozishma_bolim_text_nik").style = "background:white";
-    }else{
+    if (theme=="moon") {
       document.querySelector(".yozishma_bolim_text_nik").style = "background:black";
-    }
-    if (theme=="sun") {
-      document.querySelector(".chat-body").style = "background:white";
     }else{
+      document.querySelector(".yozishma_bolim_text_nik").style = "background:white";
+    }
+    if (theme=="moon") {
       document.querySelector(".chat-body").style = "background:black";
-    }
-    if (theme=="sun") {
-      document.querySelector(".chat-header").style = "background:white";
     }else{
+      document.querySelector(".chat-body").style = "background:white";
+    }
+    if (theme=="moon") {
       document.querySelector(".chat-header").style = "background:black";
-    }
-    if (theme=="sun") {
-      document.querySelector(".chat-window .chat-footer input").style = "background:white";
     }else{
+      document.querySelector(".chat-header").style = "background:white";
+    }
+    if (theme=="moon") {
       document.querySelector(".chat-window .chat-footer input").style = "background:black";
-    }
-    if (theme=="sun") {
-      document.querySelector(".chat-window .chat-footer button").style = "background:white";
     }else{
+      document.querySelector(".chat-window .chat-footer input").style = "background:white";
+    }
+    if (theme=="moon") {
       document.querySelector(".chat-window .chat-footer button").style = "background:black";
+    }else{
+      document.querySelector(".chat-window .chat-footer button").style = "background:white";
     }
 
     document.querySelector(".yozishma_small_div").style = "display:block;";
     document.querySelector(".chat_gap_text_div").style = "display:block;";
-    document.querySelector(".chat_yoq_payt").style = "display:none;";
+    document.querySelector(".chat_yooq").style = "display:none;";
   }
 
   function back1() {
     document.querySelector(".yozishma_bolim_text_nik").style="display:block"
     document.querySelector(".openModelAddChat").style="display:none"
-    if (theme=="sun") {
-      document.querySelector(".yozishma_bolim_text_nik").style = "background:white;color:black";
-    }else{
+    if (theme=="moon") {
       document.querySelector(".yozishma_bolim_text_nik").style = "background:black;color:white";
+    }else{
+      document.querySelector(".yozishma_bolim_text_nik").style = "background:white;color:black";
     }
   }
 function openModelAddChat() {
   document.querySelector(".yozishma_bolim_text_nik").style="display:none !important"
   document.querySelector(".openModelAddChat").style="display:block !important"
-  if (theme=="sun") {
-    document.querySelector(".openModelAddChat").style = "background:white;color:black;display:block";
-  }else{
+  if (theme=="moon") {
     document.querySelector(".openModelAddChat").style = "background:black;color:white;display:block";
+  }else{
+    document.querySelector(".openModelAddChat").style = "background:white;color:black;display:block";
   }
 }
 
@@ -182,9 +184,13 @@ function openModelAddChat() {
             })}
           </div>
           </div>
-            <div style={theme=="sun"?{background:"white",color:"black"}:{background:"black",color:"white"}} className="yozishma_bolim_text_nik">
+            <div style={theme=="moon"?{background:"black",color:"white"}:{background:"white",color:"black"}} className="yozishma_bolim_text_nik">
+              <div className="telegram_menu">
+                a
+              </div>
               <div className="yozishma_bolim_text_nik_search">
-                <input style={theme=="sun"?{background:"white"}:{background:"black"}} id="searchss" onChange={handleInputChange} type="text" placeholder="Search... " />
+              <i onClick={()=>telegrammenu()} class='bx bx-menu'></i>
+                <input style={theme=="moon"?{background:"black"}:{background:"white"}} id="searchss" onChange={handleInputChange} type="text" placeholder="Search... " />
                 <label class="switch" >
   <span class="sun"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="#ffd43b"><circle r="5" cy="12" cx="12"></circle><path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path></g></svg></span>
   <span class="moon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg></span>   
@@ -212,7 +218,7 @@ function openModelAddChat() {
                         <div className="tg_img">
                           <img src={tgimg} alt="" />
                         </div>
-                        <h1 style={theme=="sun"?{color:"black"}:{color:"white"}} id="name">{displayName.slice(0,-10)}</h1>
+                        <h1 style={theme=="moon"?{color:"white"}:{color:"black"}} id="name">{displayName.slice(0,-10)}</h1>
                   
                       </div>
                       <div className="yozishma_bolim_text_nik_text_qongiroq">
@@ -225,13 +231,14 @@ function openModelAddChat() {
               
             </div>
 
-            <div style={theme=="sun"?{background:"white"}:{background:"black"}} className="yozishma_small_div">
+            <div style={theme=="moon"?{background:"black"}:{background:"white"}} className="yozishma_small_div">
               <div className="chat_gap_text_div">
                 <Chat socket={socket} room={room} email={email} />
               </div>
+              <div className="chat_yooq">
               <div className="chat_yoq_payt">
-                <p style={theme=="sun"?{color:"black"}:{color:"white"}}>Select a chat to start messaging</p>
-              </div>
+                <p style={theme=="moon"?{color:"white"}:{color:"black"}}>Select a chat to start messaging</p>
+              </div></div>
             </div>
           </div>
         </div>
