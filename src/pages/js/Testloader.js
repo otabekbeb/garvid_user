649a1e42b6365d6f2b645c5d8322fdd6d371c543
odil3474 <137496224+odil3474@users.noreply.github.com>
2023-08-11 11:32:18 +0500

    jtfghcyh
    tbtfhc

--- src/pages/js/Testloader.js
+++ src/pages/js/Testloader.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/Testpage.css'
 import Navbar from './Navbar'
 import Footer1 from '../js/Footer1.js'
@@ -6,50 +6,50 @@ import Footer1 from '../js/Footer1.js'
 
 export default function Testloader() {
 
-    const[loading,setloading]= useState(false)
+  const [loading, setloading] = useState(false)
 
-    useEffect(()=>{
-        setloading(true);
-        setTimeout(()=>{
-            setloading(false);
-        },18000);
-    },[])
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 18000);
+  }, [])
 
   return (
     <div>
-         <Navbar/>
-<div className="letsgo-test" >
-<div className="test-ichi1">
-<div>
- 
-    <div>
-      {loading?((<div className='loader-suka'><span class="loader"></span></div>)):(<div>
+      <Navbar />
+      <div className="letsgo-test" >
+        <div className="test-ichi1">
+          <div>
 
-<div className="butonn-la">
-<div className="asaloriza">
-<div className="ruzultat-foiz">
-        <h2>25 <span>%</span> </h2>
-    </div>
-    <div className="rezultat-text">
-    <box-icon type='solid' name='bar-chart-alt-2' color="#444"></box-icon><p><span>5</span> out of <span>3</span> points</p>
-    </div>
-</div>
-<button onClick={()=>window.location="/Testpage"}>Retry</button>
-</div>
+            <div>
+              {loading ? ((<div className='loader-suka'><span class="loader"></span></div>)) : (<div>
 
-      </div>)}
+                <div className="butonn-la">
+                  <div className="asaloriza">
+                    <div className="ruzultat-foiz">
+                      <h2>25 <span>%</span> </h2>
+                    </div>
+                    <div className="rezultat-text">
+                      <box-icon type='solid' name='bar-chart-alt-2' color="#444"></box-icon><p><span>5</span> out of <span>3</span> points</p>
+                    </div>
+                  </div>
+                  <button onClick={() => window.location = "/Testpage"}>Retry</button>
+                </div>
 
- 
-    </div>
-</div>
-  
-    
+              </div>)}
 
 
-</div>
+            </div>
+          </div>
 
-</div>
-<Footer1/>
+
+
+
+        </div>
+
+      </div>
+      <Footer1 />
     </div>
   )
 }
