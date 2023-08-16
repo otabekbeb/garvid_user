1e18a5299280ff4e5c7cdf63674f1117a91d0cf4
odil3474 <137496224+odil3474@users.noreply.github.com>
2023-08-16 12:52:13 +0500

    fdsfsd
    fdfsdfsd

--- src/pages/js/Azo.js
+++ src/pages/js/Azo.js
@@ -1,14 +1,57 @@
 import React, { useState, useEffect } from 'react'
-import FollowCard from './FollowCard'
-export default function Azo() {
+import "../css/azo.css"
+import azoimg from "../img/Ellipse.jpg"
+import axios from 'axios'
+import url from './Host'
 
+export default function Azo() {
+    const [follow, setFollow] = useState([])
+    function folowcolor() {
+        document.querySelector('.followButton1').classList.toggle("followButton2")
+      }
+    useEffect(() => {
+        axios.get(`${url}/auth/teachers`, { headers: { Authorization: "Bearer " + localStorage.getItem("token") } }).then(res => {
+          setFollow(res.data)
+        })
+      }, [])
+    function obuna() {
+        document.querySelector('#azo_bolgan_katta_div_text_block_button').classList.toggle("obuna1")
+    }
+    function obuna2() {
+        document.querySelector('#azo_bolgan_katta_div_text_block_button1').classList.toggle("obuna2")
+    }
+    const [state1, setState1] = React.useState();
+    useEffect(() => {
+        setState1(
+            localStorage.getItem("lang") ? localStorage.getItem("lang") : "en"
+        );
+    }, []);
 
     return (
         <div>
 
-           <FollowCard/>
-              
            
+              
+                <div className="followcards1">
+                    {follow.map(item => {
+                        return (
+                            <a>
+                                <div className="followcard1">
+                                    <a href="/TeacherProfil">
+                                        {item.image === null ? (<h1>No signaL</h1>) : (<img className='jony_foto' src={item.image} alt="" />)}
+
+                                        <h5 className='Name'>{item.username}</h5 >
+                                    </a>
+                                    <button onClick={() => folowcolor()} className='followButton1' >subscribe</button>
+                                </div>
+                            </a>
+                        )
+                    })}
+
+
+
+                
+            </div>
 
         </div>
 
