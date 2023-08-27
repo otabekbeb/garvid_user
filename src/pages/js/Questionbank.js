8174a3d3ca3aac64d0f871b9488fad541d811b35
Akbar-07 <116638921+Akbar-07@users.noreply.github.com>
2023-08-27 11:37:03 +0500

    aa
    aa

---
+++ src/pages/js/Questionbank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "9*9",
+		options: ["64","87","82","81"],
+		answer: "81",
+	},
+	{
+		id: 2,
+		question: "7*4",
+		options: ["27","32","28","32"],
+		answer: "28",
+	},
+	{
+		id: 3,
+		question: "132+65",
+		options: ["225","197","167","184"],
+		answer: "197"
+	},
+	{
+		id: 4,
+		question: "875-561",
+		options: ["314","345","390","453"],
+		answer: "314"
+	},
+	{
+		id: 5,
+		question: "144/14",
+		options: ["12","11","14","20"],
+		answer: "14"
+	},
+]
+
+export default qBank;
