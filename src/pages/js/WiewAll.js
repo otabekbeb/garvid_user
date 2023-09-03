0cdcd64f5eb40caf7ea36635f9a58bdf3dccd61e
odil3474 <137496224+odil3474@users.noreply.github.com>
2023-09-03 17:28:19 +0500

    oiuygfd
    oiuygtf

--- src/pages/js/WiewAll.js
+++ src/pages/js/WiewAll.js
@@ -6,6 +6,7 @@ import axios from 'axios'
 import url from './Host'
 import Profil from './Profil'
 import Futer from '../js/Footer1'
+import Groupimg from "../img/Group 2.png";
 function WiewAll() {
     const [Wiew, setWiew] = useState([])
     const [Oneuser, setOneuser] = useState()
@@ -32,28 +33,44 @@ function WiewAll() {
             <h1 style={{ textAlign: 'center' }}></h1>
             <div className="wiew_div">
                 <h3 className='logo'>Notifications</h3>
-                <div className="wiew_sms_big">
-                    {Wiew.length === 0 ? (<h1 style={{ textAlign: 'center' }}> Xabar yogu akbar</h1>) : (<> {Wiew.map(item => {
-                        if (item.to_user_id == localStorage.getItem("OneuserId")) {
-                            return (
-                                <div style={{ cursor: "pointer" }} className="sms">
-                                    <div className="qizil"></div>
-                                    <button onClick={() => atvet(item.user_id)}>a</button>
-                                    <div className="data_title">
-                                        <input type="text" id='atvetu' />
-                                        <input type="text" id='atvetu1' />
-                                        <p className='unred'>{item.title}</p>
-                                        <p className='data'>{item.time_create.slice(11, 16)}</p>
-                                    </div>
-                                    <div className="p_lorem_sms">
-                                        <p className='lorem_sms'>{item.description} </p>
-                                        <input style={{ display: "none" }} type="text" id='atvet' />
-                                    </div>
-                                </div>
-                            )
-                        }
+                {/* <div className="icon">
+                    <img  className='ooo'  src={Qongro} alt="" />
+                </div>
+                <p className='qongiro_title'>You don't have any notifications yet!</p> */}
 
-                    })}</>)}
+                <div className="wiew_sms_big">
+                    {Wiew.length === 0 ? (
+                        <>   <div className="delete_padding">
+                            <img style={{ width: "30%", paddingTop: '40px' }} src={Qongro} alt="" />
+                            <h4 style={{ marginTop: '20px', fontSize: '25px' }}>No message</h4>
+                            <div className="delete_btns">
+                                <a href="/Ourcourse">
+                                    {" "}
+
+                                </a>
+                            </div>
+                        </div></>) : (<>
+                            {Wiew.map(item => {
+                                if (item.to_user_id == localStorage.getItem("OneuserId")) {
+                                    return (
+                                        <div style={{ cursor: "pointer" }} className="sms">
+                                            <div className="qizil"></div>
+                                            <button onClick={() => atvet(item.user_id)}>a</button>
+                                            <div className="data_title">
+                                                <input type="text" id='atvetu' />
+                                                <input type="text" id='atvetu1' />
+                                                <p className='unred'>{item.title}</p>
+                                                <p className='data'>{item.time_create.slice(11, 16)}</p>
+                                            </div>
+                                            <div className="p_lorem_sms">
+                                                <p className='lorem_sms'>{item.description} </p>
+                                                <input style={{ display: "none" }} type="text" id='atvet' />
+                                            </div>
+                                        </div>
+                                    )
+                                }
+
+                            })}</>)}
 
 
 
