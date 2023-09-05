23ab920920a11a70f62c3226347b48cfd6aa5687
odil3474 <137496224+odil3474@users.noreply.github.com>
2023-09-05 19:05:34 +0500

    weryuip
    sdwaefsrgthyukiop[p

--- src/pages/js/WiewAll.js
+++ src/pages/js/WiewAll.js
@@ -18,15 +18,15 @@ export default function WiewAll() {
 
     useEffect(() => {
         axios.get(`${url}/api/notification`, { headers: { Authorization: "Bearer" + localStorage.getItem("token") } }).then(res => {
-            axios.get(`${url}/auth/allusers`,{headers:{Authorization:"Bearer "+localStorage.getItem("token")}}).then(res1=>{
+            axios.get(`${url}/auth/allusers`, { headers: { Authorization: "Bearer " + localStorage.getItem("token") } }).then(res1 => {
                 for (let i = 0; i < res.data.length; i++) {
-                for (let j = 0; j < res1.data.length; j++) {
-                    if(res.data[i].user_id==res1.data[j].id){
-                      res.data[i].username=res1.data[j].username
-                      res.data[i].last_name=res1.data[j].last_name
+                    for (let j = 0; j < res1.data.length; j++) {
+                        if (res.data[i].user_id == res1.data[j].id) {
+                            res.data[i].username = res1.data[j].username
+                            res.data[i].last_name = res1.data[j].last_name
+                        }
                     }
-                }
-                    
+
                 }
                 setWiew(res.data)
             })
@@ -45,15 +45,15 @@ export default function WiewAll() {
         formdata.append("to_user_id", id)
         axios.post(`${url}/api/notification`, formdata, { headers: { Authorization: "Bearer " + localStorage.getItem("token") } }).then(res => {
             axios.get(`${url}/api/notification`, { headers: { Authorization: "Bearer" + localStorage.getItem("token") } }).then(res => {
-                axios.get(`${url}/auth/allusers`,{headers:{Authorization:"Bearer "+localStorage.getItem("token")}}).then(res1=>{
+                axios.get(`${url}/auth/allusers`, { headers: { Authorization: "Bearer " + localStorage.getItem("token") } }).then(res1 => {
                     for (let i = 0; i < res.data.length; i++) {
-                    for (let j = 0; j < res1.data.length; j++) {
-                        if(res.data[i].user_id==res1.data[j].id){
-                          res.data[i].username=res1.data[j].username
-                          res.data[i].last_name=res1.data[j].last_name
+                        for (let j = 0; j < res1.data.length; j++) {
+                            if (res.data[i].user_id == res1.data[j].id) {
+                                res.data[i].username = res1.data[j].username
+                                res.data[i].last_name = res1.data[j].last_name
+                            }
                         }
-                    }
-                        
+
                     }
                     setWiew(res.data)
                 })
@@ -69,7 +69,7 @@ export default function WiewAll() {
     function inputClose(key) {
         document.querySelectorAll("#inputNotifaction")[key].style.display = "none"
     }
-    function Page(id,key) {
+    function Page(id, key) {
         setPageId(key)
         setRead(id)
         setPage(1)
@@ -111,17 +111,18 @@ export default function WiewAll() {
                                             </div>
                                         </div>
                                     )
-                                }else if(item.to_user_id==read){
+                                } else if (item.to_user_id == read) {
                                     return (
                                         <div>
                                             <div style={{ cursor: "pointer" }} className="sms">
                                                 {/*<div className="qizil"></div>*/}
-                                                <p className='data'>{item.username}</p>
-                                                <p className='data'>{item.last_name}</p>
+                                                <p style={{marginBottom:'0px'}} className='data'>{item.username}</p>
+                                                <p  className='data'>{item.last_name}</p>
                                                 <div className="data_title">
                                                     <p className='unred'>{item.title}</p>
-                                                    <p className='data'>{item.time_create.slice(11, 16)}</p>
+                                                    
                                                 </div>
+                                                <p  className='data'>{item.time_create.slice(11, 16)}</p>
                                                 <div className="p_lorem_sms">
                                                     <p className='lorem_sms'>{item.description} </p>
                                                     <input style={{ display: "none" }} type="text" id='atvet' />
@@ -132,16 +133,16 @@ export default function WiewAll() {
                                 }
                             })} </div>)}
                             <div id='inputNotifaction' className="input_notification_bid_div">
-                            <input id='atvetu1' onChange={(e) => setInputValue(e.target.value)} type="text" />
-                            <button onClick={() => atvet(read)}>Send</button>
-                        </div>
+                                <input id='atvetu1' onChange={(e) => setInputValue(e.target.value)} type="text" />
+                                <button onClick={() => atvet(read)}>Send</button>
+                            </div>
                         </div>) : (
                         <div>
                             {Wiew.map((item, key) => {
                                 if (item.to_user_id == localStorage.getItem("OneuserId")) {
                                     return (
                                         <div>
-                                            <div onClick={() => Page(item.user_id,item.id)} style={{ cursor: "pointer" }} className="sms">
+                                            <div onClick={() => Page(item.user_id, item.id)} style={{ cursor: "pointer" }} className="sms">
                                                 <div className="qizil"></div>
                                                 <div className="data_title">
                                                     <p className='unred'>{item.username}</p>
