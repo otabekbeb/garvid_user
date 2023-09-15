daf54e8e0f9c3a1ee85453d0ea149ec51f45c8d0
odil3474 <137496224+odil3474@users.noreply.github.com>
2023-09-15 16:45:06 +0500

    frf
    rfrfrf

--- src/pages/js/Newspage.js
+++ src/pages/js/Newspage.js
@@ -11,10 +11,13 @@ import { PiCalendarBold } from 'react-icons/pi'
 export default function News() {
 
   const [base, setBase] = useState([])
+  const [base1, setBase1] = useState([])
   const [basetype, setBasetype] = useState([])
   useEffect(() => {
     axios.get(`${url}/api/knowladge`).then(res => {
       setBase(res.data)
+      const Filter = res.data.filter((item, key) => key < 2)
+      setBase1(Filter)
       console.log(res.data, "bb");
       axios.get(`${url}/api/base_theme`).then(res1 => {
         setBasetype(res1.data)
@@ -26,6 +29,11 @@ export default function News() {
     });
 
   }, [])
+  function all() {
+    axios.get(`${url}/api/knowladge`).then(res => {
+      setBase1(res.data);
+    })
+  }
   return (
 
     <div className='newspage1'>
@@ -53,48 +61,48 @@ export default function News() {
 
       <div className="newspage">
         <div className='old_news'>
-          <div className="old_news_card">
-            <div className="calendar">
-              <PiCalendarBold className='cl' />
-              <p>16:23</p>
-            </div>
-            <p className='old_news_pro'>ошланиши мумкин. UzAuto расмий вакилининг Kun.uz'га маълум қилишича, айни пайтда синов жараёни ўтказилмоқда. Нархларни сотувлар бошланишидан олдин эълон қилиш режалаштирилган.</p>
+          <div className='old_news1'>
+            {base1.map((item, key) => {
+              return (
+                <div className="old_news_card">
+                  <div className="news_chas">
+                    <div className="calendar">
+                      <PiCalendarBold className='cl' />
+                      <p>{item.time_create.slice(0, 10)}</p>
+                      {/* <p>2020-19-09</p> */}
+
+                    </div>
+                    <div className="chas">
+                      {item.time_create.slice(11, 16)}
+                      {/* <p>10:00</p> */}
+                    </div>
+                  </div>
+                  <p className='old_news_pro'>{item.description}</p>
+                  {/* <p className='old_news_pro'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum enim ut impedit autem commodi quasi reprehenderit doloremque ipsam deleniti necessitatibus suscipit, labore sit dolore nobis iste possimus facere ex voluptas.</p> */}
+                </div>
+              )
+
+            })}
+
+
           </div>
-          <div className="old_news_card">
-            <div className="calendar">
-              <PiCalendarBold className='cl' />
-              <p>16:23</p>
-            </div>
-            <p className='old_news_pro'>ошланиши мумкин. UzAuto расмий вакилининг Kun.uz'га маълум қилишича, айни пайтда синов жараёни ўтказилмоқда. Нархларни сотувлар бошланишидан олдин эълон қилиш режалаштирилган.</p>
-          </div>
-          <div className="old_news_card">
-            <div className="calendar">
-              <PiCalendarBold className='cl' />
-              <p>16:23</p>
-            </div>
-            <p className='old_news_pro'>ошланиши мумкин. UzAuto расмий вакилининг Kun.uz'га маълум қилишича, айни пайтда синов жараёни ўтказилмоқда. Нархларни сотувлар бошланишидан олдин эълон қилиш режалаштирилган.</p>
-          </div>
-          <div className="old_news_card">
-            <div className="calendar">
-              <PiCalendarBold className='cl' />
-              <p>16:23</p>
-            </div>
-            <p className='old_news_pro'>ошланиши мумкин. UzAuto расмий вакилининг Kun.uz'га маълум қилишича, айни пайтда синов жараёни ўтказилмоқда. Нархларни сотувлар бошланишидан олдин эълон қилиш режалаштирилган.</p>
-          </div>
+          <button className='all_butn' onClick={() => all()}>Wiew all</button>
         </div>
+
         <div className="news_card">
           {base.map((item, key) => {
             if (key == localStorage.getItem("baseId")) {
               return (
                 <div className="news_cards" >
                   <div className="news_img">
-                  {item.image === null ? (<img style={{ height: '555px' }} src={img_for_null} alt="" />) : (<img src={item.image} alt="" />)}
+                    {item.image === null ? (<img style={{ height: '555px' }} src={img_for_null} alt="" />) : (<img src={item.image} alt="" />)}
                   </div>
                   <div className="news_title">
                     <h4>{item.name}</h4>
                   </div>
                   <div className="news_p">
                     <p>{item.description}</p>
+                    {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam natus praesentium doloremque veritatis assumenda sunt numquam deserunt modi maiores, similique reprehenderit enim ipsam adipisci, saepe nihil iure aperiam sequi magnam?</p> */}
                   </div>
                 </div>
               )
